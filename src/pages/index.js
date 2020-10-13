d395ce4ffbfc8eeea8aebc46226afcfd48e34dfd
DumbSkull <beetaspiderman@gmail.com>
2020-10-14 02:28:18 +0530

    added navbar using material-ui slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
@@ -10,12 +10,14 @@ const IndexPage = () => (
   <Layout>
     {/* <SEO title="Home" /> */}
     <Hero></Hero>
-    {/* <h1>Hello World!</h1> */}
-    {/* <p>This marks the start of the development on the website for UpVision.</p> */}
+    <h1>Hello World!</h1>
+    <p style={{ marginBottom: "2000px" }}>
+      This marks the start of the development on the website for UpVision.
+    </p>
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+    <Link to="/page-2/">Go to page 2</Link> <br />
     {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
