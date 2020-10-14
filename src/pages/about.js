820119695b7d4f2498d77794908d67869f83e7d1
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-10-14 12:40:22 +0530

    Initial about page setup

---
+++ src/pages/about.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Layout from '../components/layout';
+import '../css/about.css'
+
+function about() {
+    return (
+        <Layout>
+            <div className="about_wrapper">
+                <div className="about_header">ABOUT US</div>
+                <div className="about_content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
+                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
+                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
+                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
+                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
+                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
+                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
+                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
+                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
+                    </p>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 import Hero from "../components/hero"
+
 
 const IndexPage = () => (
   <Layout>
@@ -15,7 +16,7 @@ const IndexPage = () => (
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+    <Link to="/about">about</Link>
     {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
