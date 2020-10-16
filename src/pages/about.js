3bdce4888d3ec1aa6e01c27354b5a9ff457299b9
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-10-16 22:11:31 +0530

    about page updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import { Link } from "gatsby"
 
@@ -31,40 +30,3 @@ const SecondPage = () => (
 )
 
 export default SecondPage
-=======
-import React from 'react'
-import Layout from '../components/layout';
-import '../css/about.css'
-
-function about() {
-    return (
-        <Layout>
-            <div className="about_wrapper">
-                <div className="about_header">ABOUT US</div>
-                <div className="about_content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
-                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
-                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
-                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
-                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
-                    </p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
-                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
-                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Fuga blanditiis, quibusdam repellat ipsam id tempore officia unde quae earum iure? Fugit labore tempora quod. 
-                    Officia eos soluta iste veritatis nesciunt deserunt ex nulla itaque dolorem laborum. Numquam, cupiditate 
-                    ipsam eligendi harum fugit ea similique autem distinctio! Beatae veritatis eos libero.
-                    </p>
-                </div>
-            </div>
-        </Layout>
-    )
-}
-
-export default about
->>>>>>> 820119695b7d4f2498d77794908d67869f83e7d1
