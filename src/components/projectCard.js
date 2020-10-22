d5a345c78ef7ffd27c8eecbf3c57467db6857f89
Blaine <rishi1998@gmail.com>
2020-10-23 04:50:08 +0530

    dynamic cards

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,39 +1,42 @@
 import React from "react"
+import arrow from "../images/arrow.svg"
 
 function ProjectCard(props) {
   const { title, description, repoLink, imageURL, contributers } = props
 
   return (
     <div>
-      <div class="container">
-        <div class="post">
-          <div class="header_post">
+      <div className="container">
+        <div className="post">
+          <div className="header_post">
             <img src={imageURL} alt="" className="main-img" />
           </div>
 
-          <div class="body_post">
-            <div class="post_content">
+          <div className="body_post">
+            <div className="post_content">
               <h1 style={{ marginBottom: "5px" }}>{title}</h1>
-              <small>Contributers: </small>
-              {contributers.map(contributer => {
-                return (
-                  <a href={contributer.profileLink} target="_blank">
-                    <img
-                      src={contributer.imageURL}
-                      alt={contributer.altTag}
-                      className="avatar"
-                    />
-                  </a>
-                )
-              })}
+              {(contributers.length > 0) && <div className="body_collaborators">
+                <small>Contributers: </small>
+                {contributers.map(contributer => {
+                  return (
+                    <a href={contributer.profileLink} target="_blank" rel>
+                      <img
+                        src={contributer.imageURL}
+                        alt={contributer.altTag}
+                        className="avatar"
+                      />
+                    </a>
+                  )
+                })}
+              </div>}
               <hr />
               <p>
                 <strong>Description - </strong>
                 {description}
               </p>
-              <a href={repoLink} style={{ float: "right" }}>
-                GO TO THE PROJECT REPO
-              </a>
+            </div>
+            <div className="card_repoUrl">
+              <a href={repoLink}><img src={arrow} alt="Repository..." width="69"/></a>
             </div>
           </div>
         </div>
