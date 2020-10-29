e2ec698eddd17471c1e384add8f735e5dfd16ec7
DumbSkull <beetaspiderman@gmail.com>
2020-10-29 22:24:32 +0530

    changed navbar; fixed routing; lazily fixed the responsiveness of members page

--- src/components/membersCard.js
+++ src/components/membersCard.js
@@ -1,39 +1,58 @@
 import React from "react"
 import "../css/members.css"
-import InstagramIcon from '@material-ui/icons/Instagram';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import Grid from "@material-ui/core/Grid";
-import GitHubIcon from '@material-ui/icons/GitHub';
-function MembersCard(props){
-    const { membername,imgprof,post,about,insta,linkedin,github}=props
-    return(
-        <>
-<Grid>
-    <div className="flip-card" style={{width: "400px",height:"370px"}}>
-    <div className="flip-card-inner">
-        <div className="flip-card-front">
-        <img src={imgprof} alt="Img" style={{width: 350, height: 350, borderRadius: 350/ 2}} />
+import InstagramIcon from "@material-ui/icons/Instagram"
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import Grid from "@material-ui/core/Grid"
+import GitHubIcon from "@material-ui/icons/GitHub"
+function MembersCard(props) {
+  const { membername, imgprof, post, about, insta, linkedin, github } = props
+  return (
+    <>
+      <Grid>
+        <div className="flip-card" style={{ width: "320px", height: "320px" }}>
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img
+                src={imgprof}
+                alt="Img"
+                style={{ width: 320, height: 320, borderRadius: 320 / 2 }}
+              />
+            </div>
+            <div
+              className="flip-card-back"
+              style={{ width: 320, height: 320, borderRadius: 320 / 2 }}
+            >
+              <div
+                style={{ textAlign: "center", margin: "6px", padding: "6px" }}
+              >
+                <br />
+                <h3 style={{ fontSize: "1.45em" }}> {membername} </h3>
+                <h4 style={{ fontSize: "1.3em" }}>{post}</h4>
+                <p>{about}</p>
+                <Grid>
+                  <a href={insta} className="icons">
+                    <InstagramIcon />
+                  </a>
+                  <a href={linkedin} className="icons">
+                    <LinkedInIcon />
+                  </a>
+                  <a href={github} className="icons">
+                    <GitHubIcon />
+                  </a>
+                </Grid>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h2
+              style={{ textAlign: "center", color: "white", marginTop: "10px" }}
+            >
+              {membername}
+            </h2>
+          </div>
         </div>
-        <div className="flip-card-back" style={{width: 350, height: 350, borderRadius: 350/2}}>
-        <div style={{textAlign: "center",margin: "6px",padding:"6px"}}>
-        <br/>
-        <h3 style={{fontSize:"1.45em"}}> {membername} </h3>
-        <h4 style={{fontSize:"1.3em"}}>{post}</h4>
-        <p>{about}</p>
-        <Grid>
-        <a href={insta} className="icons"><InstagramIcon/></a>
-        <a href={linkedin} className="icons"><LinkedInIcon/></a>
-        <a href={github} className="icons"><GitHubIcon/></a>
-        </Grid>
-        </div>
-        </div>
-    </div>
-    <div>
-    <h2 style={{textAlign:"center",color:"white"}}>{membername}</h2>
-    </div>
-    </div> 
-</Grid>
-       </>
-    );
+      </Grid>
+    </>
+  )
 }
-export default MembersCard;
+export default MembersCard
--- src/pages/members.js
+++ src/pages/members.js
@@ -1,81 +1,94 @@
 import React from "react"
-import Grid from "@material-ui/core/Grid";
-import MembersCard from "../components/membersCard";
+import Grid from "@material-ui/core/Grid"
+import MembersCard from "../components/membersCard"
 import SEO from "../components/seo"
 
 function members() {
   return (
-   <>
-<SEO title="Members" />
-<h1 className="poshead">PRESIDENT</h1>
-<Grid container direction="row" justify="space-evenly" alignItems="center">
-    <MembersCard
-        membername="JOHN WICK"
-        imgprof="https://picsum.photos/200/300"
-        post="President"
-        about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
-        insta="https://www.instagram.com/"
-        linkedin="https://www.linkedin.com/"
-        github="https://www.github.com/"
-    />
-    <MembersCard
-        membername="JOHN WICK"
-        imgprof="https://picsum.photos/200/300"
-        post="President"
-        about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
-        insta="https://www.instagram.com/"
-        linkedin="https://www.linkedin.com/"
-        github="https://www.github.com/"
-    />
-    <MembersCard
-        membername="JOHN WICK"
-        imgprof="https://picsum.photos/200/300"
-        post="President"
-        about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
-        insta="https://www.instagram.com/"
-        linkedin="https://www.linkedin.com/"
-        github="https://www.github.com/"
-    />
-</Grid>
+    <>
+      <SEO title="Members" />
+      <h1 className="poshead">PRESIDENT</h1>
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+      >
+        <MembersCard
+          membername="JOHN WICK"
+          imgprof="https://picsum.photos/200/300"
+          post="President"
+          about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
+          insta="https://www.instagram.com/"
+          linkedin="https://www.linkedin.com/"
+          github="https://www.github.com/"
+        />
+        <MembersCard
+          membername="JOHN WICK"
+          imgprof="https://picsum.photos/200/300"
+          post="President"
+          about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
+          insta="https://www.instagram.com/"
+          linkedin="https://www.linkedin.com/"
+          github="https://www.github.com/"
+        />
+        <MembersCard
+          membername="JOHN WICK"
+          imgprof="https://picsum.photos/200/300"
+          post="President"
+          about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
+          insta="https://www.instagram.com/"
+          linkedin="https://www.linkedin.com/"
+          github="https://www.github.com/"
+        />
+      </Grid>
 
+      <h1 className="poshead">VICE PRESIDENT</h1>
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+      >
+        <MembersCard
+          membername="JOHN WICK"
+          imgprof="https://picsum.photos/300/300"
+          post="President"
+          about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
+          insta="https://www.instagram.com/"
+          linkedin="https://www.linkedin.com/"
+          github="https://www.github.com/"
+        />
+        <MembersCard
+          membername="JOHN WICK"
+          imgprof="https://picsum.photos/300/300"
+          post="President"
+          about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
+          insta="https://www.instagram.com/"
+          linkedin="https://www.linkedin.com/"
+          github="https://www.github.com/"
+        />
+      </Grid>
 
-<h1 className="poshead">VICE PRESIDENT</h1>
-<Grid container direction="row" justify="space-evenly" alignItems="center">
-    <MembersCard
-        membername="JOHN WICK"
-        imgprof="https://picsum.photos/200/300"
-        post="President"
-        about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
-        insta="https://www.instagram.com/"
-        linkedin="https://www.linkedin.com/"
-        github="https://www.github.com/"
-    />
-    <MembersCard
-        membername="JOHN WICK"
-        imgprof="https://picsum.photos/200/300"
-        post="President"
-        about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
-        insta="https://www.instagram.com/"
-        linkedin="https://www.linkedin.com/"
-        github="https://www.github.com/"
-    />
-</Grid>
-
-
-<h1 className="poshead">VOLUNTEER</h1>
-<Grid container direction="row" justify="space-evenly" alignItems="center">
-<MembersCard
-        membername="JOHN WICK"
-        imgprof="https://picsum.photos/200/300"
-        post="President"
-        about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
-        insta="https://www.instagram.com/"
-        linkedin="https://www.linkedin.com/"
-        github="https://www.github.com/"
-    />
-</Grid> 
-   </>
-  );
+      <h1 className="poshead">VOLUNTEER</h1>
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+      >
+        <MembersCard
+          membername="JOHN WICK"
+          imgprof="https://picsum.photos/300/300"
+          post="President"
+          about="Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ Ex intern at XYZ"
+          insta="https://www.instagram.com/"
+          linkedin="https://www.linkedin.com/"
+          github="https://www.github.com/"
+        />
+      </Grid>
+    </>
+  )
 }
 
-export default members;
+export default members
