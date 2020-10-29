90165c53375909c6dd7553508b327a1ba52ed364
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-10-29 11:45:54 +0530

    Used Gitform for storing contact us responses

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -4,6 +4,9 @@ import '../css/contact.css'
 import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import { styled } from '@material-ui/styles';
+import axios from "axios";
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const MyButton = styled(Button)({
     background: '#7B807A',
@@ -41,6 +44,38 @@ const ContactUs = (props)=> {
     const [values, setValues] = useState(intialFieldValues);
     const classes = useStyle();
 
+    const [serverState, setServerState] = useState({
+        submitting: false,
+        status: null
+    });
+
+    const handleServerResponse = (ok, msg, form) => {
+        setServerState({
+          submitting: false,
+          status: { ok, msg }
+        });
+        if (ok) {
+          form.reset();
+        }
+    };
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        setServerState({ submitting: true });
+        axios({
+          method: "post",
+          url: "https://getform.io/f/e259a4c6-8fc3-4832-84d5-30ef0176e489",
+          data: new FormData(form)
+        })
+        .then(r => {
+        handleServerResponse(true, "Thanks!", form);
+        })
+        .catch(r => {
+        handleServerResponse(false, r.response.data.error, form);
+        });
+    };
+
     const handleInputChange = (e)=>{
         const {name, value} = e.target;
         setValues({
@@ -56,7 +91,12 @@ const ContactUs = (props)=> {
                     Contact Us
                 </div>
                 <div className="contact_content">
-                    <form className={classes.root}>
+                    <form 
+                        className={classes.root} 
+                        action="https://getform.io/f/e259a4c6-8fc3-4832-84d5-30ef0176e489" 
+                        method="POST"
+                        onSubmit={handleOnSubmit}
+                    >
                         <span className="form_label">Name:</span> 
                         <TextField 
                             variant="filled"
@@ -84,7 +124,18 @@ const ContactUs = (props)=> {
                             onChange={handleInputChange}
                             name="msg"
                         />
-                        <MyButton className="contact_button">Submit</MyButton>
+                        <MyButton 
+                            className="contact_button" 
+                            type="submit"
+                            disabled={serverState.submitting}
+                        >Submit</MyButton>
+                        <div className="contact_msg">
+                            {serverState.status && (
+                                <p className={!serverState.status.ok ? "ErrorMsg" : ""}>
+                                {serverState.status.msg}
+                                </p>
+                            )}
+                        </div>
                     </form>
                 </div>
             </div>
