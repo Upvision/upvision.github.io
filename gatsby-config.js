983b1c2ef8268e21f639d4c923e4a3f1187c1c41
Blaine <rishi1998@gmail.com>
2020-10-31 08:08:07 +0530

    use new logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#13e8b5`,
+        theme_color: `#13e8b5`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
