f0e75bb191126b8bff46aa99dcae19d441099c56
Blaine <rishi1998@gmail.com>
2020-10-31 08:12:27 +0530

    several bug fixes

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import TextField from '@material-ui/core/TextField';
 import '../css/contact.css'
 import { makeStyles } from '@material-ui/core';
